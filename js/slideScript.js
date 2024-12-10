03c58936613308a04bb81d1822391059f2f72678
Iago-Peres <138594888+Iago-Peres@users.noreply.github.com>
2024-12-09 21:02:31 -0300

    Add files via upload

--- js/slideScript.js
+++ js/slideScript.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
     $('#projetos-container').click(() => {
-        let content = $(this).next('#projetos');
+        let content = $('#projetos');
+        let img = $('#collapse-img');
+
+        if (img.attr('src') === "imgs/collapse.png") {
+            img.attr('src', 'imgs/collapse_down.png');
+        } else if (img.attr('src') === "imgs/collapse_down.png") {
+            img.attr('src', 'imgs/collapse.png');
+        }
 
         content.slideToggle(300, function () {
             if (content.is(':visible')) {
                 content.css({
-                    'display': 'flex',
-                    'flex-flow': 'row wrap'
+                    'display': 'grid'
                 });
             } else {
                 content.css('display', 'none');
